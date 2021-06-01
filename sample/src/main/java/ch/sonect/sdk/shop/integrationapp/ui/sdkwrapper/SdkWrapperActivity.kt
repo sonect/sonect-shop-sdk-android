0f1608c35f76ec3b592105e02385327fbeeb1062
Ryazantsev Dmitry <dilix90@gmail.com>
2021-06-01 11:19:25 +0500

    * master: no message
    Jira issue
    https://sonectapp.atlassian.net/browse/* master

--- sample/src/main/java/ch/sonect/sdk/shop/integrationapp/ui/sdkwrapper/SdkWrapperActivity.kt
+++ sample/src/main/java/ch/sonect/sdk/shop/integrationapp/ui/sdkwrapper/SdkWrapperActivity.kt
@@ -99,10 +99,6 @@ class SdkWrapperActivity : AppCompatActivity(), ActivityResultStorage {
                 )
             )
             .sdkCallbacks(object : SdkActionsCallback {
-                override fun comparePasscode(passcode: String): Boolean {
-                    return true
-                }
-
                 override fun onSdkLastFragmentClosed() {
                     finish()
                 }
